b4598811c05b402acde99f7b77fdabb403c6ab57
Joshua Sailsbery <jsailsbery@genedx.com>
2023-05-15 19:06:15 -0400

    adding grep

---
+++ tests/tools/test_grep.py
@@ -0,0 +1,59 @@
+import os
+import tempfile
+import pytest
+from universal.tools import Grep
+
+@pytest.fixture(scope='module')
+def test_files():
+    # Create temporary files for testing
+    with tempfile.TemporaryDirectory() as temp_dir:
+        file1 = os.path.join(temp_dir, 'file1.txt')
+        file2 = os.path.join(temp_dir, 'file2.txt')
+        file3 = os.path.join(temp_dir, 'file3.txt')
+
+        # Write some content to the files
+        with open(file1, 'w') as f:
+            f.write('Hello, World!\nThis is a test file.\n')
+
+        with open(file2, 'w') as f:
+            f.write('Testing Grep class\nSearching for patterns\n')
+
+        with open(file3, 'w') as f:
+            f.write('This file does not contain the pattern.\n')
+
+        yield [file1, file2, file3]
+
+def test_search_files(test_files):
+    # Test search_files method
+    directory = os.path.dirname(test_files[0])
+    pattern = '*.txt'
+
+    result = list(Grep.search_files(directory, pattern))
+    assert len(result) == 3
+    assert test_files[0] in result
+    assert test_files[1] in result
+    assert test_files[2] in result
+
+def test_grep(test_files, caplog):
+    # Test grep method
+    pattern = 'test'
+    files = iter(test_files)
+
+    with caplog.at_level('INFO'):
+        lines = Grep.grep(pattern, files)
+
+        assert len(lines) == 2
+        assert 'file1.txt:2:This is a test file.' in lines[0]
+        assert 'file2.txt:1:Testing Grep class' in lines[1]
+
+def test_grep_all_files(test_files, caplog):
+    # Test grep_all_files method
+    pattern = 'test'
+    directory = os.path.dirname(test_files[0])
+
+    with caplog.at_level('INFO'):
+        lines = Grep.grep_all_files(pattern, directory)
+        assert len(lines) == 2
+
+        assert 'file1.txt:2:This is a test file.' in lines[0]
+        assert 'file2.txt:1:Testing Grep class' in lines[1]
--- universal/tools/grep.py
+++ universal/tools/grep.py
@@ -8,19 +8,19 @@ logging.basicConfig(level=logging.INFO, format='%(message)s')
 logger = logging.getLogger(__name__)
 
 
-class GrepTool:
+class Grep:
     """
-    A utility class for searching and matching patterns in files within a directory and its subdirectories.
+    A utility class for searching and matching patterns in files within a source_dir and its subdirectories.
     This class provides methods for searching files based on a file name pattern, searching for a text
-    pattern within files, and combining both operations to grep all files in a directory.
+    pattern within files, and combining both operations to grep all files in a source_dir.
     """
 
     @classmethod
     def search_files(cls, directory: str, pattern: str, case_sensitive: bool = False) -> Iterator[str]:
         """
-        Search for files matching a pattern in a directory and its subdirectories.
+        Search for files matching a pattern in a source_dir and its subdirectories.
 
-        :param directory: The directory to start the search from.
+        :param directory: The source_dir to start the search from.
         :param pattern: The pattern to search for in file names.
         :param case_sensitive: Whether the search should be case-sensitive. Defaults to False.
         :return: An iterator of file paths matching the pattern.
@@ -29,7 +29,7 @@ class GrepTool:
             pattern = pattern.lower()
 
         if not os.path.isdir(directory):
-            logger.error(f"Provided path is not a directory: {directory}")
+            logger.error(f"Provided path is not a source_dir: {directory}")
             return iter([])
 
         try:
@@ -59,9 +59,10 @@ class GrepTool:
             try:
                 with open(file_path, "r", errors='ignore') as file:
                     for line_number, line in enumerate(file, 1):
+                        line = line.strip()
                         if regex.search(line):
-                            lines.append(line)
-                            logger.info(f"{file_path}:{line_number}:{line.strip()}")
+                            lines.append(f"{file_path}:{line_number}:{line}")
+                            #logger.info(f"{file_path}:{line_number}:{line.strip()}")
             except (FileNotFoundError, PermissionError, OSError) as e:
                 logger.error(f"Error processing file {file_path}: {str(e)}")
 
@@ -70,14 +71,14 @@ class GrepTool:
     @classmethod
     def grep_all_files(cls, pattern: str, directory: str) -> List[str]:
         """
-        Search for a pattern in all files within a directory and its subdirectories.
+        Search for a pattern in all files within a source_dir and its subdirectories.
 
         This method combines the functionality of search_files and grep methods to search for a
-        text pattern in all files within the specified directory and its subdirectories.
+        text pattern in all files within the specified source_dir and its subdirectories.
 
         :param pattern: The pattern to search for.
-        :param directory: The directory to start the search from.
+        :param directory: The source_dir to start the search from.
         :return: A list of lines containing the pattern from all matching files.
         """
-        all_files = GrepTool.search_files(directory, "*")
+        all_files = Grep.search_files(directory, "*")
         return cls.grep(pattern, all_files)
