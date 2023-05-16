def4d269651fe8a1e30bc6398cafa46c609100db
Joshua Sailsbery <jsailsbery@genedx.com>
2023-05-16 12:43:44 -0400

    updated remote actions and its tests

--- tests/remote/test_remote_dir_actions.py
+++ tests/remote/test_remote_dir_actions.py
@@ -9,14 +9,15 @@ def test_exists():
     """Test RemoteDirActions.exists method"""
     temp_dir = tempfile.mkdtemp()
     with patch('universal.remote.RemoteCommand.execute') as mock_execute:
-        mock_execute.return_value = PropertyMock(success=True, completion=True)
+        mock_execute.return_value = PropertyMock(success=True, completion=True, errors=False)
         assert RemoteDirActions.exists(temp_dir, SSHConfig()) is True
-        mock_execute.return_value = PropertyMock(success=False, completion=True)
+        mock_execute.return_value = PropertyMock(success=False, completion=True, errors=False)
         assert RemoteDirActions.exists(temp_dir, SSHConfig()) is False
         mock_execute.return_value = PropertyMock(completion=False)
         with pytest.raises(RemoteExecuteException):
             RemoteDirActions.exists(temp_dir, SSHConfig())
     os.rmdir(temp_dir)
+
 
 def test_create():
     """Test RemoteDirActions.create method"""
@@ -24,29 +25,31 @@ def test_create():
     os.rmdir(temp_dir)
     with patch('universal.remote.RemoteDirActions.isdir', return_value=False) as mock_isdir:
         with patch('universal.remote.RemoteCommand.execute') as mock_execute:
-            mock_execute.return_value = PropertyMock(success=True, completion=True)
+            mock_execute.return_value = PropertyMock(success=True, completion=True, errors=False)
             result = RemoteDirActions.create(temp_dir, SSHConfig())
             assert result == True
-            mock_execute.return_value = PropertyMock(success=False, completion=True)
+            mock_execute.return_value = PropertyMock(success=False, completion=True, errors=False)
             result = RemoteDirActions.create(temp_dir, SSHConfig())
             assert result == False
             mock_execute.return_value = PropertyMock(completion=False)
             with pytest.raises(RemoteExecuteException):
                 RemoteDirActions.create(temp_dir, SSHConfig())
 
+
 def test_delete():
     """Test RemoteDirActions.delete method"""
     temp_dir = tempfile.mkdtemp()
     with patch('universal.remote.RemoteDirActions.exists', return_value=True) as mock_exists:
         with patch('universal.remote.RemoteCommand.execute') as mock_execute:
-            mock_execute.return_value = PropertyMock(success=True, completion=True)
+            mock_execute.return_value = PropertyMock(success=True, completion=True, errors=False)
             assert RemoteDirActions.delete(temp_dir, SSHConfig()) == True
-            mock_execute.return_value = PropertyMock(success=False, completion=True)
+            mock_execute.return_value = PropertyMock(success=False, completion=True, errors=False)
             assert RemoteDirActions.delete(temp_dir, SSHConfig()) == False
             mock_execute.return_value = PropertyMock(completion=False)
             with pytest.raises(RemoteExecuteException):
                 RemoteDirActions.delete(temp_dir, SSHConfig())
     os.rmdir(temp_dir)
+
 
 def test_list():
     """Test RemoteDirActions.list method"""
@@ -55,12 +58,13 @@ def test_list():
         mock_execute.return_value = PropertyMock(success=True, completion=True, stdout='lrwxrwxrwx 1 user group 9 May 12 10:30 link -> target')
         with patch('universal.remote.RemoteFileActions.follow', return_value=1024):
             assert RemoteDirActions.list(temp_dir, SSHConfig()) == {'target': 1024}
-        mock_execute.return_value = PropertyMock(success=False, completion=True)
+        mock_execute.return_value = PropertyMock(success=False, completion=True, errors=False)
         assert RemoteDirActions.list(temp_dir, SSHConfig()) == None
         mock_execute.return_value = PropertyMock(completion=False)
         with pytest.raises(RemoteExecuteException):
             RemoteDirActions.list(temp_dir, SSHConfig())
     os.rmdir(temp_dir)
+
 
 def test_copy():
     """Test RemoteDirActions.copy method"""
@@ -69,9 +73,9 @@ def test_copy():
     with patch('universal.remote.RemoteDirActions.exists', return_value=True) as mock_exists:
         with patch('universal.remote.RemoteDirActions.create', return_value=True) as mock_create:
             with patch('universal.remote.RemoteCommand.execute') as mock_execute:
-                mock_execute.return_value = PropertyMock(success=True, completion=True)
+                mock_execute.return_value = PropertyMock(success=True, completion=True, errors=False)
                 assert RemoteDirActions.copy(source_dir, destination_dir, SSHConfig()) == True
-                mock_execute.return_value = PropertyMock(success=False, completion=True)
+                mock_execute.return_value = PropertyMock(success=False, completion=True, errors=False)
                 assert RemoteDirActions.copy(source_dir, destination_dir, SSHConfig()) == False
                 mock_execute.return_value = PropertyMock(completion=False)
                 with pytest.raises(RemoteExecuteException):
@@ -79,14 +83,15 @@ def test_copy():
     os.rmdir(source_dir)
     os.rmdir(destination_dir)
 
+
 def test_remove():
     """Test RemoteDirActions.remove method"""
     temp_dir = tempfile.mkdtemp()
     with patch('universal.remote.RemoteDirActions.exists', return_value=True) as mock_exists:
         with patch('universal.remote.RemoteCommand.execute') as mock_execute:
-            mock_execute.return_value = PropertyMock(success=True, completion=True)
+            mock_execute.return_value = PropertyMock(success=True, completion=True, errors=False)
             assert RemoteDirActions.remove(temp_dir, SSHConfig()) == True
-            mock_execute.return_value = PropertyMock(success=False, completion=True)
+            mock_execute.return_value = PropertyMock(success=False, completion=True, errors=False)
             assert RemoteDirActions.remove(temp_dir, SSHConfig()) == False
             mock_execute.return_value = PropertyMock(completion=False)
             with pytest.raises(RemoteExecuteException):
